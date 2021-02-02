fac65487e8b3c46f2e8a49b673deb3218a9c25fe
Lina Do <l.do5147@gmail.com>
2021-02-01 17:30:30 -0800

    Initial Commit

---
+++ What's Cookin'/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  What's Cookin'
+//
+//  Created by Lina Do on 2/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ What's Cookin'/What_s_Cookin_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_s_Cookin_App.swift
+//  What's Cookin'
+//
+//  Created by Lina Do on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct What_s_Cookin_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
