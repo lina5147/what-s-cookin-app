293f630a785c921f38ae0292fa32c2b2c709d953
Lina Do <l.do5147@gmail.com>
2021-02-01 20:27:22 -0800

    buiding search form page, enter ingredient field and adding to the ingredient list

--- What's Cookin'/ContentView.swift
+++ What's Cookin'/ContentView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var ingredients = [String]()
+    @State private var ingredient = ""
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      NavigationView {
+        Form {
+          Section {
+            HStack {
+              TextField("Enter Ingredient", text: $ingredient)
+                .multilineTextAlignment(TextAlignment.center)
+                
+              Button(action: {
+                if ingredient != "" {
+                  self.ingredients.append(ingredient)
+                }
+                self.ingredient = ""
+                print(ingredients)
+              }) {Image(systemName: "plus")
+                .font(.largeTitle)
+                .foregroundColor(.blue)}
+            }
+
+          }
+          Section {
+              ForEach(ingredients, id: \.self) { ingredient in
+                  Text(ingredient)
+              }
+          }
+        }
+      }
     }
-}
+  }
+
+
+
+//class IngredientsList : ObservableObject {
+//  @Published var ingrediants = [String]()
+//
+//  init() {
+//    addIngrediant
+//  }
+//
+//
+//
+//
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
