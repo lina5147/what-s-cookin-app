54c97f925d38cbce4a93cccb9f261e8a73d67f4b
Lina Do <l.do5147@gmail.com>
2021-02-05 13:14:02 -0800

    got the details page to connect with API

--- What's Cookin'/Views/RecipeDetails.swift
+++ What's Cookin'/Views/RecipeDetails.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 import URLImage
 
+struct Details: Codable, Hashable {
+  var ingredients: [String]
+  var instructions: [String]
+}
+
+
 struct RecipeDetails: View {
+  @State var extraDetails = [Details]()
     var id: Int
     var title: String
     var image: String
+  
   
     var body: some View {
       ZStack{
@@ -30,7 +38,10 @@ struct RecipeDetails: View {
                   .padding(.top, 25.0)
                   .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
                   .multilineTextAlignment(.center)
+              Section {
+              }
               Spacer()
+
             }
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 700)
                 .background(Color.white)
@@ -44,10 +55,33 @@ struct RecipeDetails: View {
               .padding(.top, 18.0)
 
    
-        }
+        }.onAppear(perform: loadData)
       }
       .navigationBarTitle("Recipe", displayMode: .inline).font(.title2)
+      
     }
+  
+  func loadData() {
+
+    guard let url = URL(string: "http://127.0.0.1:5000/search/\(self.id)") else {
+        print("Invalid URL")
+        return
+    }
+    print(url)
+    let request = URLRequest(url: url)
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      if let data = data {
+        if let decodedResponse = try? JSONDecoder().decode([Details].self, from: data) {
+          DispatchQueue.main.async {
+            self.extraDetails = decodedResponse
+            print(decodedResponse)
+          }
+          return
+        }
+      }
+      print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+    }.resume()
+  }
 }
 
 struct RecipeDetails_Previews: PreviewProvider {
